4a321f35d4e7b27126934b6749edbe2e6b889bb9
Mazerty <mazerty@gmail.com>
2016-01-27 12:48:37 +0100

    nettoyage du pom

--- src/main/java/fr/mazerty/shika/torii/MyUI.java
+++ src/main/java/fr/mazerty/shika/torii/MyUI.java
@@ -1,43 +1,30 @@
 package fr.mazerty.shika.torii;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
 
-/**
- *
- */
+import javax.servlet.annotation.WebServlet;
+
 @Theme("mytheme")
-@Widgetset("fr.mazerty.shika.torii.MyAppWidgetset")
 public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
-        
+
         final TextField name = new TextField();
         name.setCaption("Type your name here:");
 
         Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
+        button.addClickListener(e -> layout.addComponent(new Label("Thanks " + name.getValue() + ", it works!")));
+
         layout.addComponents(name, button);
         layout.setMargin(true);
         layout.setSpacing(true);
-        
+
         setContent(layout);
     }
 
@@ -45,4 +32,5 @@ public class MyUI extends UI {
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
     }
+
 }
