9495c14bb6cef29ab3a04558bc38eb9d21ba498d
Mazerty <mazerty@gmail.com>
2016-02-20 16:35:33 +0100

    utilisation de javaslang + création de myview + enrichissement de session et workflow de loginview

--- src/main/java/fr/mazerty/shika/torii/MyBeanFieldGroup.java
+++ src/main/java/fr/mazerty/shika/torii/MyBeanFieldGroup.java
@@ -1,17 +1,21 @@
 package fr.mazerty.shika.torii;
 
 import com.vaadin.data.fieldgroup.BeanFieldGroup;
+import javaslang.control.Try;
 
 public class MyBeanFieldGroup<T> extends BeanFieldGroup<T> {
 
     public MyBeanFieldGroup(Class<T> beanType) {
         super(beanType);
+        setBuffered(false);
 
-        try {
-            setItemDataSource(beanType.newInstance());
-        } catch (ReflectiveOperationException e) {
-            e.printStackTrace(); // TODO
-        }
+        Try.of(beanType::newInstance)
+                .onFailure(Throwable::printStackTrace) // TODO
+                .andThen(this::setItemDataSource);
+    }
+
+    public T getBean() {
+        return getItemDataSource().getBean();
     }
 
 }
