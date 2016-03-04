7151f393a3f1911551518b6912a96060eba39ca5
Mazerty <mazerty@gmail.com>
2016-03-04 07:33:01 +0100

    poursuite de la factorisation dans shika-ishi et suppression de l'objet application afin de simplifier l'applicationproducer

--- src/main/java/fr/mazerty/shika/torii/ApplicationDao.java
+++
@@ -1,9 +0,0 @@
-package fr.mazerty.shika.torii;
-
-import fr.mazerty.shika.ishi.Application;
-
-public interface ApplicationDao {
-
-    Application selectByName(String name);
-
-}
--- src/main/java/fr/mazerty/shika/torii/ApplicationProducer.java
+++ src/main/java/fr/mazerty/shika/torii/ApplicationProducer.java
@@ -1,24 +1,12 @@
 package fr.mazerty.shika.torii;
 
-import fr.mazerty.shika.ishi.Application;
-import org.mybatis.cdi.Mapper;
-
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
-import javax.inject.Named;
 
 public class ApplicationProducer {
 
-    @Mapper
-    @Inject
-    private ApplicationDao applicationDao;
-
-    @Named
-    @ApplicationScoped
     @Produces
-    public Application getApplication() {
-        return applicationDao.selectByName("torii");
+    public String getApplication() {
+        return "torii";
     }
 
 }
