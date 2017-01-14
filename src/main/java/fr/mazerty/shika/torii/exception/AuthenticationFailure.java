9ba3a6f399cd6d65554d2c7c21986fa242397ef3
mazerty <mazerty@gmail.com>
2017-01-14 16:24:44 +0000

    todo

--- src/main/java/fr/mazerty/shika/torii/exception/AuthenticationFailure.java
+++ src/main/java/fr/mazerty/shika/torii/exception/AuthenticationFailure.java
@@ -3,7 +3,7 @@ package fr.mazerty.shika.torii.exception;
 public class AuthenticationFailure extends Exception {
 
     public AuthenticationFailure() {
-        super("Wrong email or password");
+        super("Wrong email or password"); // TODO localization
     }
 
 }
