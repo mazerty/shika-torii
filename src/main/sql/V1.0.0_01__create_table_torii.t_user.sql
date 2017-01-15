8b99915e024c6e91022b40a420b6d2254d1f8520
mazerty <mazerty@gmail.com>
2017-01-15 07:44:38 +0000

    simplify user structure since we have a single app now

--- src/main/sql/V1.0.0_01__create_table_torii.t_user.sql
+++ src/main/sql/V1.0.0_01__create_table_torii.t_user.sql
@@ -2,6 +2,7 @@ CREATE TABLE torii.t_user (
   id       INTEGER NOT NULL,
   email    TEXT    NOT NULL,
   password TEXT    NOT NULL,
+  admin    BOOLEAN NOT NULL,
 
   PRIMARY KEY (id),
   UNIQUE (email)
@@ -9,5 +10,5 @@ CREATE TABLE torii.t_user (
 
 CREATE SEQUENCE torii.s_user_id;
 
-INSERT INTO torii.t_user (id, email, password)
-VALUES (nextval('torii.s_user_id'), 'admin', '$2a$13$byD4Ftv39Z76hUfd01URsePSdaV722c7J7NcLfs6o3KdJsAwHhEjq');
+INSERT INTO torii.t_user (id, email, password, admin)
+VALUES (nextval('torii.s_user_id'), 'admin', '$2a$13$byD4Ftv39Z76hUfd01URsePSdaV722c7J7NcLfs6o3KdJsAwHhEjq', TRUE);
