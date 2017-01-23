17ce65df113b9e4fdce90b5625888e891cfdaf37
Mazerty <mazerty@gmail.com>
2017-01-23 06:57:27 +0000

    moving mainwindow stuff into mainview to get rid of non-modal windows

--- src/main/java/fr/mazerty/shika/torii/vaadin/MainView.java
+++ src/main/java/fr/mazerty/shika/torii/vaadin/MainView.java
@@ -2,21 +2,44 @@ package fr.mazerty.shika.torii.vaadin;
 
 import com.vaadin.cdi.CDIView;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import fr.mazerty.shika.ishi.vaadin.MyGrid;
 import fr.mazerty.shika.ishi.vaadin.MyView;
+import fr.mazerty.shika.torii.bean.User;
 
-import javax.inject.Inject;
+import javax.annotation.PostConstruct;
 
 @CDIView(MainView.MAIN_VIEW_NAME)
 public class MainView extends MyView {
 
     static final String MAIN_VIEW_NAME = "main";
 
-    @Inject
-    private MainWindow mainWindow;
+    @PostConstruct
+    public void postConstruct() {
+        // TODO : i18n when moved to admin view
+        setCaption("Users");
+
+        Button add = new Button("Add");
+
+        HorizontalLayout horizontalLayout = new HorizontalLayout(add);
+        horizontalLayout.setHeightUndefined();
+        horizontalLayout.setWidth(100, Unit.PERCENTAGE);
+        horizontalLayout.setComponentAlignment(add, Alignment.MIDDLE_RIGHT);
+
+        MyGrid grid = new MyGrid<>(User.class);
+        grid.setSizeFull();
+
+        addComponents(horizontalLayout, grid);
+        setSizeFull();
+        setMargin(true);
+        setSpacing(true);
+        setExpandRatio(grid, 1);
+    }
 
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent event) {
-        show(mainWindow);
     }
 
 }
