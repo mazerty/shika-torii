16c72a33e08e21512785e39734ffad6f372bea39
Mazerty <mazerty@gmail.com>
2017-01-25 18:29:43 +0000

    javadoc

--- src/main/java/fr/mazerty/shika/torii/vaadin/MyUI.java
+++ src/main/java/fr/mazerty/shika/torii/vaadin/MyUI.java
@@ -14,6 +14,9 @@ import fr.mazerty.shika.torii.cdi.Session;
 
 import javax.inject.Inject;
 
+/**
+ * Custom {@link UI}, entrypoint of the application
+ */
 @CDIUI("")
 @Title("Torii")
 @Theme("mytheme")
@@ -25,8 +28,12 @@ public class MyUI extends UI {
     @Inject
     private Session session;
 
+    /**
+     * Entrypoint method of the application
+     */
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        // defines that the application consists of a set of annotated views you can access through the ui's navigator
         Navigator navigator = new Navigator(this, this);
         navigator.addProvider(cdiViewProvider);
         navigator.addViewChangeListener(new MyViewChangeListener());
@@ -34,21 +41,28 @@ public class MyUI extends UI {
         navigator.navigateTo(LoginView.VIEW_NAME);
     }
 
+    /**
+     * The heart of the application's security. Every change of view is challenged here against the user's permissions.
+     */
     private class MyViewChangeListener implements ViewChangeListener {
 
         @Override
         public boolean beforeViewChange(ViewChangeEvent event) {
+            // popups are not tied to the views, which is a shame, so we have to close them before moving on
             getWindows().forEach(Window::close);
 
             boolean goingToLoginView = LoginView.VIEW_NAME.equals(event.getViewName());
             boolean goingToAdminView = AdminView.VIEW_NAME.equals(event.getViewName());
 
             if (goingToLoginView) {
+                // anyone can access the login view but doing so will log you out
                 session.logout();
                 return true;
-            } else if (!goingToAdminView && session.isLoggedIn() || session.isAdmin()) {
+            } else if (session.isAdmin() || session.isLoggedIn() && !goingToAdminView) {
+                // an admin can go anywhere, so does a normal user except for the admin view
                 return true;
             } else {
+                // any other attempt is forbidden and redirects to the login view
                 getNavigator().navigateTo(LoginView.VIEW_NAME);
                 return false;
             }
